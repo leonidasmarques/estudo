196e3ffaf82da82bfc2f9e1780122141e87f1984
Leonida <leonidasmmarques@outlook.com>
2023-05-27 18:28:27 -0300

    primeiro commit

---
+++ estudo/src/entities/Retangulo.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Retangulo {
+	
+	public double largura;
+	public double altura;
+	
+	
+	public double area(double largura, double altura) {
+		return largura * altura;
+	}
+
+}
